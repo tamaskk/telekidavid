0e2b248ce4edfb06025b4eb7a9b8367cad8e66da
Tamas Krisztian Kalman <kalman.tamaskrisztian@gmail.com>
2024-08-29 17:43:13 +0200

    fixing

--- components/Cookie.tsx
+++ components/Cookie.tsx
@@ -12,13 +12,14 @@ const Cookie = ({
   return (
     <div className="fixed bottom-0 left-0 z-[999999] bg-black p-4 flex flex-col max-sm:gap-4 max-sm:text-center sm:flex-row justify-center items-center">
       <p>{t("cookie_text")}</p>
+      <div className="flex flex-row">
       <button
         onClick={() => {
           localStorage.setItem("cookieAccepted", "true");
           setIsAccepted(true);
         }}
         className="ml-4 bg-white text-black px-4 py-2 rounded"
-      >
+        >
         {t("cookie_accept")}
       </button>
       <button
@@ -26,9 +27,10 @@ const Cookie = ({
           localStorage.setItem("cookieAccepted", "true");
         }}
         className="ml-4 bg-white text-black px-4 py-2 rounded"
-      >
+        >
         {t("cookie_decline")}
       </button>
+        </div>
     </div>
   );
 };
