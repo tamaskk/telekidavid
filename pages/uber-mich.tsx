3f38a10809063510295cdd65ef01af58765b4dbf
Tamas Krisztian Kalman <kalman.tamaskrisztian@gmail.com>
2024-09-02 19:48:54 +0200

    fix

--- pages/uber-mich.tsx
+++ pages/uber-mich.tsx
@@ -3,7 +3,7 @@ import UberMinch from "@/components/Uber";
 
 export default function Uber() {
   return (
-    <main className="w-screen h-screen overflow-hidden">
+    <main className="w-screen h-screen overflow-x-hidden overflow-y-auto">
       <Nav />
       <div className="w-screen h-screen flex flex-col items-center justify-center">
         <UberMinch />
