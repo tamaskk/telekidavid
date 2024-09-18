c024955a813ec739074c68553a4ca01ca3835988
Tamas Krisztian Kalman <kalman.tamaskrisztian@gmail.com>
2024-09-18 11:02:34 +0200

    fixing

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,6 +11,40 @@ export default function Document() {
           data-blockingmode="auto"
           type="text/javascript"
         ></script>
+
+        {/* Google Tag Manager Script */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-XXQH15Y9L4"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-XXQH15Y9L4');
+            `,
+          }}
+        />
+
+        {/* Facebook Pixel Script */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '2116730415336406');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
       </Head>
       <body>
         <Main />
