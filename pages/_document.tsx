88732f6171e5ff33d47bb81bb74411981863a260
Tamas Krisztian Kalman <kalman.tamaskrisztian@gmail.com>
2024-09-18 10:55:42 +0200

    fixing

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,17 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          id="Cookiebot"
+          src="https://consent.cookiebot.com/uc.js"
+          data-cbid="13f87ab3-6883-40ca-9f24-1a9e56dc0daf"
+          data-blockingmode="auto"
+          type="text/javascript"
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
